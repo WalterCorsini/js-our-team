f824a0013d7d59115c1af4f4432f6d50366e4552
WalterCorsini <corsini.walter@gmail.com>
2024-04-11 15:29:17 +0200

    milestone 0: project scaffolding and create object array

---
+++ js/script.js
@@ -0,0 +1,35 @@
+// declaration object array
+
+const team =[
+    {
+        name: "Wayne Barnett",
+        profession: "Founder & CEO",
+        photo: "./img/wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        name: "Angela Caroll",
+        profession: "Chief Editor",
+        photo: "./img/angela-caroll-chief-editor.jpg" 
+    },
+    {
+        name: "Walter Gordon",
+        profession: "Office Manager",
+        photo: "./img/walter-gordon-office-manager.jpg"
+    },
+    {
+        name: "Angela Lopez",
+        profession: "Social Media Manager",
+        photo: "./img/angela-lopez-social-media-manager.jpg"
+    },
+    {
+        name: "Scott Estrada",
+        profession: "Developer",
+        photo: "./img/scott-estrada-developer.jpg"
+    },
+    {
+        name: "Barbara Ramos",
+        profession: "Graphic Designer",
+        photo: "./img/barbara-ramos-graphic-designer.jpg"
+    }
+]
+
