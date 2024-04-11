b29e2e5f940227148bce99747b2fd7ad5ccf02d3
WalterCorsini <corsini.walter@gmail.com>
2024-04-11 15:57:28 +0200

    milestone 2: show in html value of the object key

--- js/script.js
+++ js/script.js
@@ -4,46 +4,49 @@ const team =[
     {
         name: "Wayne Barnett",
         profession: "Founder & CEO",
-        photo: "./img/wayne-barnett-founder-ceo.jpg"
+        photo: "wayne-barnett-founder-ceo.jpg"
     },
     {
         name: "Angela Caroll",
         profession: "Chief Editor",
-        photo: "./img/angela-caroll-chief-editor.jpg" 
+        photo: "angela-caroll-chief-editor.jpg" 
     },
     {
         name: "Walter Gordon",
         profession: "Office Manager",
-        photo: "./img/walter-gordon-office-manager.jpg"
+        photo: "walter-gordon-office-manager.jpg"
     },
     {
         name: "Angela Lopez",
         profession: "Social Media Manager",
-        photo: "./img/angela-lopez-social-media-manager.jpg"
+        photo: "angela-lopez-social-media-manager.jpg"
     },
     {
         name: "Scott Estrada",
         profession: "Developer",
-        photo: "./img/scott-estrada-developer.jpg"
+        photo: "scott-estrada-developer.jpg"
     },
     {
         name: "Barbara Ramos",
         profession: "Graphic Designer",
-        photo: "./img/barbara-ramos-graphic-designer.jpg"
+        photo: "barbara-ramos-graphic-designer.jpg"
     }
 ]
 
 for(let i=0; i<team.length; i++){
     const currElem = team[i];
-    // console.log(currElem.name);                                     // dot notation
-    // console.log(currElem.profession);
-    // console.log(currElem.photo);
-    console.log("--------------------------------------------");
     // console.log(currElem["name"]);                                 // brak notation
     // console.log(currElem["profession"]);
     // console.log(currElem["photo"]);
-    for(let elem in currElem){                                         //for in
-        
+    // console.log(currElem.name);
+    // console.log(currElem.profession);
+    // console.log(currElem.photo);
+    document.getElementById("result").innerHTML += "<div>";
+    for(let elem in currElem){                                         //for in    
         console.log(currElem[elem]);
+        document.getElementById("result").innerHTML += `<span>${currElem[elem]}</span><br>`
     }
+    document.getElementById("result").innerHTML += "</div><br>"
+    console.log("--------------------------------------------");
+    
 }
